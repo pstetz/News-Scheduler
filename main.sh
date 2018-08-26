5b0e369f888273eba7e85fb323e5efe4df096929
pstetz <pstetz@berkeley.edu>
2018-08-26 16:16:28 -0700

    Taylor help

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-(crontab -l && echo "*/5 20 * * *  python3 ~/git/News-Scheduler/audio") | crontab -
+(crontab -l && echo "*/5 20 * * *  /anaconda3/bin/python3 ~/git/News-Scheduler/audio") | crontab -
